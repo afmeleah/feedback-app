9b490b052e43825f499a2320d3e6a8eb91c510b1
Anthony Meleah <anthony.meleah@gmail.com>
2021-12-19 15:55:43 -0500

    update and edit functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
-import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from './components/FeedbackStats'
-import {useState} from "react"
+import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './pages/AboutPage'
+import AboutIconLink from './components/AboutIconLink'
+import {FeedbackProvider} from './context/FeedbackContext'
 
 function App() {
-
-  const [feedback, setFeedback] = useState(FeedbackData)
-  const deleteFeedback = (id) => {
-    if(window.confirm('Are you sure you want to delete?')) {
-    setFeedback(feedback.filter((item) => item.id !== id))
-    }
-  }
   return (
-    <>
-    <Header />
-    <div className='container'>
-      <FeedbackStats feedback={feedback}/>
-      <FeedbackList 
-        feedback={feedback}
-        handleDelete={deleteFeedback} />
-    </div>
-    </>
+    <FeedbackProvider>
+      <Router>
+      <Header text='D&D Feedback' />
+      <div className='container'>
+        <Routes>
+          <Route exact path='/' element={
+            <>
+              <FeedbackForm />
+              <FeedbackStats />
+              <FeedbackList />
+            </>
+          }>
+
+          </Route>
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
+        <AboutIconLink />
+      </div>
+      </Router>
+    </FeedbackProvider>
+    
   );
 }
 
